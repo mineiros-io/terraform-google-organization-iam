6acdb223e26fe8c06c0b1ee324f49913d4ad0255
Bence Wax <bence.wax@mineiros.io>
2022-09-26 11:47:19 +0200

    chore: update tests and from module template

---
+++ test/unit-disabled/main.tf
@@ -0,0 +1,24 @@
+module "test" {
+  source = "../.."
+
+  module_enabled = false
+
+  org_id = "unit-disabled-${local.random_suffix}"
+  # add all required arguments
+
+  # add all optional arguments that create additional/extended resources
+}
+
+module "test2" {
+  source = "../.."
+
+  module_enabled = false
+
+  org_id = "unit-disabled-${local.random_suffix}"
+  # add all required arguments
+
+  authoritative = false
+  members       = ["user:member@example.com"]
+
+  # add all optional arguments that create additional/extended resources
+}
